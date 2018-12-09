0963e06fb110f81d53064c0f71d35575871acdcf
Brande George <george.brande@gmail.com>
2018-12-09 10:57:50 +0200

    mici modif pe home -stiri : get retry - if else content

--- angular-m/src/app/core/home/home.component.ts
+++ angular-m/src/app/core/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Stire } from 'src/app/model/stire';
 import { StiriService } from 'src/app/services/stiri.service';
+import { throwError, interval, of } from 'rxjs';
+import { retryWhen, flatMap } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-home',
@@ -10,14 +13,26 @@ import { StiriService } from 'src/app/services/stiri.service';
 export class HomeComponent implements OnInit {
 
   stiri: Stire[];
+  error;
 
   constructor(private stiriService: StiriService) { }
 
   ngOnInit() {
-    this.stiriService.getStiri().subscribe(data => {
-      // console.log(data);
-      this.stiri = data.content;
-    });
+    this.stiriService.getStiri()
+    .pipe(retryWhen( data => {
+      return interval(5000)
+      .pipe(
+        flatMap(count => count === 3 ? throwError('giving up') 
+        : of(count))
+      );
+    } ))
+    .subscribe(
+      result => {
+        this.stiri = result.content;
+      },
+      err => {
+        this.error = err;
+      });
   }
 
 }
